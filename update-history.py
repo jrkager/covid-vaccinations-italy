37f8f5eca506a046289453e4b1fe8e10fb8cbef5
jrkager <dschotze@gmail.com>
2021-01-10 22:03:58 +0100

    generalized for a list of different regions

--- update-history.py
+++ update-history.py
@@ -59,64 +59,72 @@ def load_csv(filename):
         return columns
 
 savefile_all = "vacc-history/regioni-history.json"
-savefile_st_calc = "vacc-history/P.A. Bolzano.csv"
+regions_to_consider = ["P.A. Bolzano", "Sardegna"]
+savefiles_calc_base = "vacc-history"
 inhabitants_file = "popolazione.json"
+
 date_vaccination_start = "2020-12-27"
+
 header = ["d","vcc","sum_1d","sum_monotone_1d","sum_2d","sum_monotone_2d","perc_doses",
     "perc_inh_1d", "perc_inh_monotone_1d", "perc_inh_2d", "perc_inh_monotone_2d", "date"]
+
+
+# -- load population numbers --
+try:
+    with open(inhabitants_file) as f:
+        inhabitants=json.load(f)
+except:
+    print("No population data file found ({})".format(inhabitants_file))
+    inhabitants = {k : -1 for k in regions_to_consider}
 
 # -- get new data --
 regjs = scraper.get_region_json()
 today = datetime.today().strftime('%Y-%m-%d')
 regjs["date"] = today
 
-# -- check specigic region --
-if not os.path.exists(savefile_st_calc):
-    with open(savefile_st_calc,"w") as f:
-        f.write(",".join(header) + "\n")
-        f.write(",".join(["0"]*len(header)) + "\n")
-loaded = load_csv(savefile_st_calc)
+# -- check specific region --
+for region_name in regions_to_consider:
+    savefile_calc = os.path.join(savefiles_calc_base, region_name + ".csv")
+    if not os.path.exists(savefile_calc):
+        with open(savefile_calc,"w") as f:
+            f.write(",".join(header) + "\n")
+            day_before_start = (datetime.fromisoformat(date_vaccination_start) -
+                                        timedelta(days=1)).strftime('%Y-%m-%d')
+            f.write(",".join(["0"]*(len(header)-1)) + "," + day_before_start + "\n")
+    loaded = load_csv(savefile_calc)
 
-region_name = os.path.splitext(os.path.basename(savefile_st_calc))[0]
-try:
-    with open(inhabitants_file) as f:
-        r=json.load(f)
-        inhabitants = r[region_name]
-except Exception as e:
-    print(e)
-    inhabitants = -1
-today_count = regjs[region_name][0]
-perc_of_doses = regjs[region_name][1]
-print("Today counter:",today_count)
+    today_count = regjs[region_name][0]
+    perc_of_doses = regjs[region_name][1]
+    print("Today counter {}: {}".format(region_name,today_count))
 
-# -- update calculations for specific region --
-# if not enough rows, fill with interpolation from the last inserted day up to today
-# (1 entry per day since vaccination start)
-diff = (datetime.fromisoformat(today)-datetime.fromisoformat(date_vaccination_start)).days
-days_of_vacc = diff + 1
-missing_days = days_of_vacc - (len(loaded["vcc"]) - 1)
-if missing_days > 0:
-    print("{}: add values for {} day(s)".format(region_name, missing_days))
-    interpolation_count = map(int,
-                     np.linspace(loaded["vcc"][-1], today_count, missing_days+1)[1:])
-    interpolation_perc = [perc_of_doses] * missing_days
-    interpolation_date = map(lambda ds: ds.strftime('%Y-%m-%d'),
-                [datetime.fromisoformat(loaded["date"][-1]) + timedelta(days=delta)
-                      for delta in range(0, missing_days+1)][1:])
-    for count, perc, date in zip(interpolation_count,
-                                 interpolation_perc,
-                                 interpolation_date):
-        # add row to csv data
-        add_row(loaded, count, perc, inhabitants, date)
-elif loaded["vcc"][-1] != today_count or loaded["perc_doses"][-1] != perc_of_doses:
-    # if we started the script for a second time this day, substitute last line with new data
-    print("{}: subsitute today with updated calculations".format(region_name))
-    subst_last_row(loaded, today_count, perc_of_doses, inhabitants, today)
-with open(savefile_st_calc, "w") as f:
-    cwr = csv.writer(f)
-    cwr.writerow(header)
-    for row in zip(*[loaded[k] for k in header]):
-        cwr.writerow(row)
+    # -- update calculations for specific region --
+    # if not enough rows, fill with interpolation from the last inserted day up to today
+    # (1 entry per day since vaccination start)
+    diff = (datetime.fromisoformat(today)-datetime.fromisoformat(date_vaccination_start)).days
+    days_of_vacc = diff + 1
+    missing_days = days_of_vacc - (len(loaded["vcc"]) - 1)
+    if missing_days > 0:
+        print("{}: add values for {} day(s)".format(region_name, missing_days))
+        interpolation_count = map(int,
+                         np.linspace(loaded["vcc"][-1], today_count, missing_days+1)[1:])
+        interpolation_perc = [perc_of_doses] * missing_days
+        interpolation_date = map(lambda ds: ds.strftime('%Y-%m-%d'),
+                    [datetime.fromisoformat(loaded["date"][-1]) + timedelta(days=delta)
+                          for delta in range(0, missing_days+1)][1:])
+        for count, perc, date in zip(interpolation_count,
+                                     interpolation_perc,
+                                     interpolation_date):
+            # add row to csv data
+            add_row(loaded, count, perc, inhabitants[region_name], date)
+    elif loaded["vcc"][-1] != today_count or loaded["perc_doses"][-1] != perc_of_doses:
+        # if we started the script for a second time this day, substitute last line with new data
+        print("{}: subsitute today with updated calculations".format(region_name))
+        subst_last_row(loaded, today_count, perc_of_doses, inhabitants[region_name], today)
+    with open(savefile_calc, "w") as f:
+        cwr = csv.writer(f)
+        cwr.writerow(header)
+        for row in zip(*[loaded[k] for k in header]):
+            cwr.writerow(row)
 
 
 # -- update all-regions-stats file --
