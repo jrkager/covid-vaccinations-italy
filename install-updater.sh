0911a8abd5cd0dabfff61eb69eb9a35077fa3054
jrkager <dschotze@gmail.com>
2021-01-10 22:13:12 +0100

    more verbose

--- install-updater.sh
+++ install-updater.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo "Pulling..."
 git pull
 
 crontab -l > mycron
-echo "*/30 6-23 * * * sh $(pwd)/loadvacc.sh" >> mycron
+echo "*/30 * * * * sh $(pwd)/loadvacc.sh" >> mycron
 crontab mycron
 rm mycron
+echo "crontab installed to run every 30 minutes"
