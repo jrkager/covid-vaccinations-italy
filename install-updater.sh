d5879157c0baef3ae2e33107a354d00895d1d5f9
jrkager <dschotze@gmail.com>
2021-01-10 23:27:42 +0100

    cronjob fixed to first cd to repo directory

--- install-updater.sh
+++ install-updater.sh
@@ -4,7 +4,7 @@ echo "Pulling..."
 git pull
 
 crontab -l > mycron
-echo "*/30 * * * * sh $(pwd)/loadvacc.sh" >> mycron
+echo "*/30 * * * * cd $(pwd) && sh $(pwd)/loadvacc.sh" >> mycron
 crontab mycron
 rm mycron
 echo "crontab installed to run every 30 minutes"
