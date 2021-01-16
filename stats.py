83048ab9aa27b5be6c6e6c7baa0a9749fb15be10
jrkager <dschotze@gmail.com>
2021-01-16 01:53:59 +0100

    code for plots

--- stats.py
+++ stats.py
@@ -1,6 +1,11 @@
 import json
 from operator import itemgetter
 import argparse
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import dates as mdates
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument('actions', metavar='action', type=str, nargs='*',
@@ -50,3 +55,39 @@ for i, (reg, *rates) in enumerate(sortdata):
 	l = [f"{r*100: {space}.2f}%" for r in rates]
 	print(f" {i+1:2}) {reg:>21}:" + "".join(l))
 print()
+
+if input("Proceed with plot? (y,[n]) ") == "y":
+    regfiles = []
+    for f in os.listdir("vacc-history/"):
+        if f.endswith(".csv"):
+            regfiles.append(f)
+    regfiles = sorted(regfiles)
+    print("Choose region:")
+    for i, f in enumerate(regfiles):
+        print(f"{i+1:>2}: {f[:f.find('.csv')]}")
+    #try:
+    if True:
+        ch = int(input("Choice: "))
+        if ch < 1 or ch > len(regfiles):
+            raise
+        data = np.genfromtxt(f"vacc-history/{regfiles[ch-1]}", delimiter=",", usecols=(1,-1), converters={11 : lambda s: datetime.strptime(s.decode("utf-8"), "%Y-%m-%d")})
+        sum_doses = [s[0] for s in data[2:]]
+        dates = [s[1] for s in data[2:]]
+        if sum_doses[-1] == sum_doses[-2]:
+            sum_doses = sum_doses[:-1]
+            dates = dates[:-1]
+
+        x = dates
+        y = sum_doses
+        locator=mdates.AutoDateLocator(minticks=1,maxticks=10)
+        formatter=mdates.ConciseDateFormatter(locator)
+        fig,ax=plt.subplots()
+        ax.set_ylim(bottom=0,top=max(y)*1.2)
+        ax.xaxis.set_major_locator(locator)
+        ax.xaxis.set_major_formatter(formatter)
+        ax.plot(x,y,'-o')
+        plt.grid(True)
+        plt.title("Doses given - " + regfiles[ch-1][:regfiles[ch-1].find('.csv')])
+        plt.show()
+    # except:
+    #     pass
