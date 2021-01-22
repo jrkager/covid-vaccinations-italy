34f2fa9d092be5700627b74f6c344f8ef8a45961
jrkager <dschotze@gmail.com>
2021-01-23 00:07:45 +0100

    new option: distance between 1d and 2d

--- stats.py
+++ stats.py
@@ -11,7 +11,7 @@ import pandas as pd
 parser = argparse.ArgumentParser()
 parser.add_argument('actions', metavar='action', type=str, nargs='*',
                     help='choose which statistics you want to have (in order list gets ordered): \n\
-                    vacc, suppl, used, vacc1, vacc2')
+                    vacc, suppl, used, vacc1, vacc2, period')
 
 args = parser.parse_args()
 
@@ -24,14 +24,17 @@ actions = { "vacc" : # n vaccines in percentage to total population
             "vacc1" :
                 lambda vals: vals.perc_inh_1d,
             "vacc2" :
-                lambda vals: vals.perc_inh_2d
+                lambda vals: vals.perc_inh_2d,
+            "period" :
+                lambda vals: vals.period
         }
 
 order = {    "vacc" : -1,
             "suppl" : -1,
             "used" : -1,
             "vacc1" : -1,
-            "vacc2" : -1
+            "vacc2" : -1,
+            "period" : -1
         }
 
 name = {    "vacc" : "tot of pop.",
@@ -39,32 +42,53 @@ name = {    "vacc" : "tot of pop.",
             "used" : "of doses",
             "vacc1" : "1d of pop.",
             "vacc2" : "2d of pop.",
+            "period" : "dose dist"
         }
 
+tostring = { "vacc" : # n vaccines in percentage to total population
+                lambda v, s: f"{v: {s}.2f}%",
+            "suppl" : # this ratio of population could get vaccined (2 doses) with current supply
+                lambda v, s: f"{v: {s}.2f}%",
+            "used" : # doses used out of supplied ones
+                lambda v, s: f"{v: {s}.2f}%",
+            "vacc1" :
+                lambda v, s: f"{v: {s}.2f}%",
+            "vacc2" :
+                lambda v, s: f"{v: {s}.2f}%",
+            "period" :
+                lambda v, s: f" {v: >{s}}",
+        }
+
+
 if "all" in args.actions:
-    args.actions.extend(k for k in ["vacc1", "vacc2", "used", "suppl"] if k not in args.actions)
+    args.actions.extend(k for k in ["vacc1", "vacc2", "used", "suppl", "period"] if k not in args.actions)
 args.actions = [k for k in args.actions if k in actions]
 if not args.actions:
-    args.actions = ["vacc2", "vacc", "used"]
+    args.actions = ["vacc2", "vacc", "used", "period"]
 
 cont = {}
 savefile_path = "vacc-history/"
 for f in os.listdir("vacc-history/"):
     if f.endswith(".csv"):
+        reg_long = os.path.splitext(os.path.basename(f))[0]
         fc = pd.read_csv(os.path.join(savefile_path, f))
-        cont[os.path.splitext(os.path.basename(f))[0]] = fc.iloc[-1]
+        c = fc.iloc[-1].copy()
+        c["period"] = fc.sum_2d.shape[0] - \
+                        fc[fc.sum_1d > fc.sum_2d.iloc[-1]].index[0]
+        cont[reg_long] = c
+
 
 data = cont.items()
 
 sortdata = sorted(data, key = lambda s: [order[act] * actions[act](s[1]) for act in args.actions])
 
-sortdata = [(s[0], *[actions[act](s[1]) for act in args.actions]) for s in sortdata]
+sortdata = [(s[0], *zip(args.actions, [actions[act](s[1]) for act in args.actions])) for s in sortdata]
 
 print()
 space=13
 print(f" {'':2}  {'reg':>21} " + "".join([f"{name[r]:>{space+1}}" for r in args.actions]))
 for i, (reg, *rates) in enumerate(sortdata):
-    l = [f"{r: {space}.2f}%" for r in rates]
+    l = [tostring[r[0]](r[1], space) for r in rates]
     print(f" {i+1:2}) {reg:>21}:" + "".join(l))
 print()
 
@@ -82,7 +106,7 @@ if input("Proceed with plot? (y,[n]) ") == "y":
         ch = int(input("Choice: "))
         if ch < 1 or ch > len(regfiles):
             raise
-        data = np.genfromtxt(f"vacc-history/{regfiles[ch-1]}", delimiter=",", usecols=(1,-1), converters={11 : lambda s: datetime.strptime(s.decode("utf-8"), "%Y-%m-%d")})
+        data = np.genfromtxt(f"vacc-history/{regfiles[ch-1]}", delimiter=",", usecols=(3,-1), converters={17 : lambda s: datetime.strptime(s.decode("utf-8"), "%Y-%m-%d")})
         sum_doses = [s[0] for s in data[2:]]
         dates = [s[1] for s in data[2:]]
         if sum_doses[-1] == sum_doses[-2]:
